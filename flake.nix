5bb2e344663cd41a2c6e8b41d6a705c117724aba
Alex Rice <alexrice999@hotmail.co.uk>
2021-01-24 13:30:00 +0000

    Move things around

--- flake.nix
+++ flake.nix
@@ -14,11 +14,11 @@
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
     buildAgda = rev: sha256: hpkgs.callPackage ./build-agda { inherit rev sha256; };
   in rec {
-    agdaPackages-master = pkgs.callPackage ./packages/master {
+    agdaPackages-master = pkgs.callPackage ./src/master {
       Agda = buildAgda "e12f391d2539b62a62d18aef74149a9a4695a871" "1lk2ayj87kclwcj83hkw7hg6mdli9fy8wk57hjrbm2b74rk2jw7f";
     };
     agda-master = agdaPackages-master.agda;
-    agdaPackages-2_6_1_2 = pkgs.callPackage ./packages/2_6_1_2 {
+    agdaPackages-2_6_1_2 = pkgs.callPackage ./src/2_6_1_2 {
       Agda = buildAgda "v2.6.1.2" "02fl52vx2izbzkky78r26gj28sjgrk2yh79z7l4kxn467myvly3r";
     };
     agda-2_6_1_2 = agdaPackages-2_6_1_2.agda;
---
+++ src/agdaPackages.nix
@@ -0,0 +1,11 @@
+let
+  buildDir = import ../build-support/buildDir.nix;
+in {
+  standard-library = buildDir ./packages/standard-library;
+  iowa-stdlib = buildDir ./packages/iowa-stdlib;
+  agda-prelude = buildDir ./packages/agda-prelude;
+  agda-categories = buildDir ./packages/agda-categories;
+  cubical = buildDir ./packages/cubical;
+  functional-linear-algebra = buildDir ./packages/functional-linear-algebra;
+  generic = buildDir ./packages/generic;
+}
