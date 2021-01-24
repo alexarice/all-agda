d4ef1b4cf265335d845c3bac145205a3713cfb9a
Alex Rice <alexrice999@hotmail.co.uk>
2021-01-24 20:33:55 +0000

    Add agda-categories

--- src/2_6_1_2/default.nix
+++ src/2_6_1_2/default.nix
@@ -5,7 +5,7 @@ let
     let
       callPackage = self.callPackage;
       inherit (callPackage ../../build-support/agda {
-        inherit (Agda) agda agda-mode li;
+        inherit (Agda) agda agda-mode;
         inherit self;
         inherit (pkgs.haskellPackages) ghcWithPackages;
       }) withPackages mkDerivation;
@@ -14,27 +14,29 @@ let
       inherit mkDerivation;
       buildGitHub = callPackage ../../build-support/buildGitHub.nix { };
 
+      all = withPackages (builtins.filter (x: x ? isAgdaDerivation) (lib.attrValues (removeAttrs self ["all"])));
+
       agda = withPackages [ ] // { inherit withPackages; };
 
       standard-library = self.standard-library-1_4;
 
-      standard-library-1_4 = callPackage apkgs.standard-library."v1.4" {
+      standard-library-1_4 = callPackage apkgs.standard-library."1.4" {
         inherit (pkgs.haskellPackages) ghcWithPackages;
       };
 
-      standard-library-1_3 = callPackage apkgs.standard-library."v1.3" {
+      standard-library-1_3 = callPackage apkgs.standard-library."1.3" {
         inherit (pkgs.haskellPackages) ghcWithPackages;
       };
 
-      standard-library-1_5_rc1 = callPackage apkgs.standard-library."v1.5-rc1" {
+      standard-library-1_5_rc1 = callPackage apkgs.standard-library."1.5-rc1" {
         inherit (pkgs.haskellPackages) ghcWithPackages;
       };
 
       # agda-prelude = callPackage ../development/libraries/agda/agda-prelude { };
 
-      # agda-categories = callPackage ../development/libraries/agda/agda-categories { };
+      agda-categories = callPackage apkgs.agda-categories."0.1.4" { };
 
-      cubical = callPackage apkgs.cubical."v0.2" { };
+      cubical = callPackage apkgs.cubical."0.2" { };
 
       # functional-linear-algebra = callPackage
       #   ../development/libraries/agda/functional-linear-algebra { };
--- src/master/default.nix
+++ src/master/default.nix
@@ -5,7 +5,7 @@ let
     let
       callPackage = self.callPackage;
       inherit (callPackage ../../build-support/agda {
-        inherit (Agda) agda agda-mode li;
+        inherit (Agda) agda agda-mode;
         inherit self;
         inherit (pkgs.haskellPackages) ghcWithPackages;
       }) withPackages mkDerivation;
@@ -13,6 +13,8 @@ let
     {
       inherit mkDerivation;
       buildGitHub = callPackage ../../build-support/buildGitHub.nix { };
+
+      all = withPackages (builtins.filter (x: x ? isAgdaDerivation) (lib.attrValues (removeAttrs self ["all"])));
 
       agda = withPackages [ ] // { inherit withPackages; };
 
