fa9c789aee3138f2d97a5174c7558f7e94e81a52
Alex Rice <alexrice999@hotmail.co.uk>
2022-03-07 12:05:19 +0000

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -5,11 +5,12 @@
     haskellNix = {
       url = "github:input-output-hk/haskell.nix";
     };
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, haskellNix, flake-utils }:
+  outputs = { self, nixpkgs, haskellNix, flake-utils, ... }:
   flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       overlays = [ haskellNix.overlay ];
