15ba11629af4946c5ce7dd42e74f66efdd58e9b2
Alex Rice <alexrice999@hotmail.co.uk>
2023-12-05 14:34:35 +0000

    Make 1.7.3 primary

--- build-support/buildLibrarySet/default.nix
+++ build-support/buildLibrarySet/default.nix
@@ -3,6 +3,6 @@ let
   filteredVersions = lib.filterAttrs (n: v: builtins.elem aversion v.agda) attrSet;
   set = lib.mapAttrs (n: v: callPackage (import v.dir apkgs."${name}"."${n}") (lib.mapAttrs (ni: vi: self."${ni}"."${vi}") v.dep)) filteredVersions;
   versions = lib.attrNames set;
-  primaryVersion = if versions == [ ] then null else lib.last (lib.naturalSort versions);
+  primaryVersion = if versions == [ ] then null else lib.last (lib.naturalSort (builtins.filter (x: ! (lib.strings.hasInfix "rc" x)) versions));
 in
 if primaryVersion == null then null else set."${primaryVersion}" // set // { isLibrarySet = true; }
